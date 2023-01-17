35f71d07f5a34c90984a8565b0e5c72cac0827fd
Yihsiang Liow <yliow@ccis.edu>
2023-01-17 17:08:31 -0500

    no msg

---
+++ 2023-01-17/main.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+
+int main()
+{
+    std::cout << "^ ^\n"
+              << "0 0\n"
+              << " v \n"   
+              << "---\n";
+    std::cout << "bye\n";
+    
+    return 0;
+}
