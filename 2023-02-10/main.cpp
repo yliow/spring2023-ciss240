edc863796e4daafa609863b22aa4a44567a1932f
Yihsiang Liow <yliow@ccis.edu>
2023-02-10 14:53:49 -0500

    no msg

--- 2023-02-10/main.cpp
+++ 2023-02-10/main.cpp
@@ -5,6 +5,17 @@ int main()
     int gun0_pos = -3;
     int gun1_pos = -5;
     int range = 2;
+    int person_pos;
+    std::cin >> person_pos;
+
+    bool in_range0 = (gun0_pos - range <= person_pos &&
+                      person_pos <= gun0_pos + range);
+
+    bool in_range1 = (gun1_pos - range <= person_pos &&
+                      person_pos <= gun1_pos + range);
+        
+    bool in_range = in_range0 || in_range1;
+    std::cout << in_range << '\n';
     
     return 0;
 }
