b69ca189e82bb6a308a533c858df7bceac7d3d12
Yihsiang Liow <yliow@ccis.edu>
2023-02-14 15:34:40 -0500

    no msg

---
+++ 2023-02-14/main.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+
+int main()
+{
+    // std::cout << "Enter your age: ";
+    // int age;
+    // std::cin >> age;
+    // if (age < 18)
+    // {
+    //     std::cout << "You are lying. Thank you can fool me???\n";
+    // }
+
+    int x0, x1;
+    std::cin >> x0 >> x1;
+
+    if (x0 > x1)
+    {
+        std::cout << "First is bigger\n";
+    }
+    
+    return 0;
+}
