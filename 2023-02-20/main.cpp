3faf5fff898e202a02c42340a3c1613683e874ca
Yihsiang Liow <yliow@ccis.edu>
2023-02-20 14:43:39 -0500

    no msg

---
+++ 2023-02-20/main.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+
+int main()
+{
+    int x0, x1, x2, x3;
+    std::cin >> x0 >> x1 >> x2 >> x3;
+
+    int t; // for the swaps
+    
+    std::cout << "PASS 1\n";
+    if (x1 < x0)
+    {
+        t = x0; x0 = x1; x1 = t;
+    }
+    std::cout << x0 << ' ' << x1 << ' ' << x2 << '\n';
+    if (x2 < x1)
+    {
+        t = x1; x1 = x2; x2 = t;
+    }
+    std::cout << x0 << ' ' << x1 << ' ' << x2 << '\n';
+    if (x3 < x2)
+    {
+        t = x2; x2 = x3; x3 = t;
+    }
+    std::cout << x0 << ' ' << x1 << ' ' << x2 << '\n';
+
+    return 0;
+}
