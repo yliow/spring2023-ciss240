aa6a8a1778514d7a4d32d8dd01a5e379074ccb18
Yihsiang Liow <yliow@ccis.edu>
2023-02-20 14:45:46 -0500

    no msg

--- 2023-02-20/main.cpp
+++ 2023-02-20/main.cpp
@@ -24,5 +24,17 @@ int main()
     }
     std::cout << x0 << ' ' << x1 << ' ' << x2 << '\n';
 
+    std::cout << "PASS 1\n";
+    if (x1 < x0)
+    {
+        t = x0; x0 = x1; x1 = t;
+    }
+    std::cout << x0 << ' ' << x1 << ' ' << x2 << '\n';
+    if (x2 < x1)
+    {
+        t = x1; x1 = x2; x2 = t;
+    }
+    std::cout << x0 << ' ' << x1 << ' ' << x2 << '\n';
+
     return 0;
 }
