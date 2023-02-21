31ac4e344658c5884a757c65a29d9ce87f0a1798
Yihsiang Liow <yliow@ccis.edu>
2023-02-21 15:58:41 -0500

    no msg

--- 2023-02-21/main.cpp
+++ 2023-02-21/main.cpp
@@ -1,16 +1,60 @@
 #include <iostream>
+#include <cstdlib>
+#include <ctime>
 
 int main()
 {
-    int a;
-    std::cin >> a;
-    if (a == 42)
+    srand((unsigned int) time(NULL));
+    
+    // int a;
+    // std::cin >> a;
+    // if (a == 42)
+    // {
+    //     std::cout << "that's my fav number too!!!\n";
+    // }
+    // else
+    // {
+    //     std::cout << "don't u like 42???\n";
+    // }
+
+    // int n;
+    // std::cin >> n;
+    // if (n % 2 == 0)
+    // {
+    //     std::cout << "even!!!\n";
+    // }
+    // else
+    // {
+    //     std::cout << "odd!!!\n";
+    // }
+
+    // std::cout << "What is the product of 97 and 94? ";
+    // int guess;
+    // std::cin >> guess;
+
+    // if (guess == 9118)
+    // {
+    //     std::cout << "You smart dawg!\n";
+    // }
+    // else
+    // {
+    //     std::cout << "Nope ... it's 9118.\n";
+    // }
+
+    int x = rand() % 10 + 90;
+    int y = rand() % 10 + 90;
+    int answer = x * y;
+    std::cout << "What is the product of " << x << " and " << y << "? ";
+    int guess;
+    std::cin >> guess;
+
+    if (guess == answer)
     {
-        std::cout << "that's my fav number too!!!\n";
+        std::cout << "You smart dawg!\n";
     }
-    if (a != 42)
+    else
     {
-        std::cout << "don't u like 42???\n";
+        std::cout << "Nope ... it's " << answer << ".\n";
     }
     
     return 0;
