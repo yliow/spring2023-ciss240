3f21ddcd73c80481baf09b6f0b1a94769391e7fa
Yihsiang Liow <yliow@ccis.edu>
2023-02-21 15:30:33 -0500

    no msg

---
+++ 2023-02-21/main.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+
+int main()
+{
+    int a;
+    std::cin >> a;
+    if (a == 42)
+    {
+        std::cout << "that's my fav number too!!!\n";
+    }
+    if (a != 42)
+    {
+        std::cout << "don't u like 42???\n";
+    }
+    
+    return 0;
+}
