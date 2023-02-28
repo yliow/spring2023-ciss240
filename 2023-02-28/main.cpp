be8efa9bb4f6723cb4ff6c61fa148f0d2e133004
Yihsiang Liow <yliow@ccis.edu>
2023-02-28 12:20:00 -0500

    no msg

---
+++ 2023-02-28/main.cpp
@@ -0,0 +1,21 @@
+/*
+               dracula castle
+                     |S
+                     |
+                     |
+             W       |N       E
+      -------- middle of road ------                        
+     /               |S             \
+    /                |               \
+   |                 |                |
+  N|                 |N               |N
+pond ------ beginning of road ------- shack
+
+ */
+#include <iostream>
+
+int main()
+{
+    
+    return 0;
+}
