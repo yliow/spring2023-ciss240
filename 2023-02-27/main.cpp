d6109c797b74fc704176908e413560db5f2bc6f2
Yihsiang Liow <yliow@ccis.edu>
2023-02-28 11:40:39 -0500

    no msg

--- 2023-02-27/main.cpp
+++ 2023-02-27/main.cpp
@@ -2,18 +2,63 @@
 
 int main()
 {
-    int x = -3, y = 1, z = 2;
+    // int x = -3, y = 1, z = 2;
 
-    if (x >= 0)
-    {
-        y = x + z;
-    }
-    else
-    {
-        y = x - z;
-    }
+    // if (x >= 0)
+    // {
+    //     y = x + z;
+    // }
+    // else
+    // {
+    //     y = x - z;
+    // }
 
-    std::cout << y << '\n';
+    // y = (x >= 0 ? x + z : x - z);
+    // std::cout << (x >= 0 ? x + z : x - z) << '\n';
+    // std::cout << y << '\n';
+
+    // int a;
+    // std::cin >> a;
+    // int abs_a;
+    // if (a >= 0)
+    // {
+    //     abs_a = a;
+    // }
+    // else
+    // {
+    //     abs_a = -a;
+    // }
+    // abs_a = (a >= 0 ? a : -a);
+    
+    // std::cout << abs_a << '\n';
+
+    int x;
+    std::cin >> x;
+    // if (x == 0)
+    // {
+    //     std::cout << "ham\n";
+    // }
+    // else if (x == 1)
+    // {
+    //     std::cout << "eggs\n";
+    // }
+    // else
+    // {
+    //     std::cout << "sausage\n";
+    // }
+
+    switch (x)
+    {
+        case 0:
+            std::cout << "ham\n";
+            //break;
+        case 5:
+            std::cout << "eggs\n";
+            //break;
+        default:
+            std::cout << "sausage\n";
+            break;
+    }
     
     return 0;
 }
