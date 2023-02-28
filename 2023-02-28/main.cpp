3ae16bcfe74c91191e67e0c8ac4d20cd37aea6c4
Yihsiang Liow <yliow@ccis.edu>
2023-02-28 15:41:32 -0500

    no msg

--- 2023-02-28/main.cpp
+++ 2023-02-28/main.cpp
@@ -16,23 +16,37 @@ pond ------ beginning of road ------- shack
 
 int main()
 {
+    
     const int BEGINNING_OF_ROAD = 1;
     const int SHACK = 2;
     
     int my_pos = BEGINNING_OF_ROAD;
     char input;
 
-    if (my_pos == BEGINNING_OF_ROAD)
+    for (int i = 0; i < 3; i = i + 1)
     {
-        std::cout << "You are at the beginning of the road\n";
-        std::cout << "Options: E (go east), ....\n";
-        std::cin >> input;
-        std::cout << "input is " << input << '\n';
+        std::cout << "i:" << i << '\n';
+        if (my_pos == BEGINNING_OF_ROAD)
+        {
+            std::cout << "You are at the beginning of the road\n";
+            std::cout << "Options: E (go east), ....\n";
+            std::cin >> input;
+            if (input == 'E')
+            {
+                my_pos = SHACK;
+            }
+        }
+        else if (my_pos == SHACK)
+        {
+            std::cout << "You are at the shack\n";
+            std::cout << "Options: W (go west), ....\n";
+            std::cin >> input;
+            if (input == 'W')
+            {
+                my_pos = BEGINNING_OF_THE_ROAD;
+            }
+        }
+        std::cout << my_pos << '\n';
     }
-    else if (my_pos == SHACK)
-    {
-        std::cout << "You are at the shack\n";        
-    }
-    
     return 0;
 }
