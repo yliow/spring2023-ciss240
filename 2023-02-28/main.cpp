50a430929f0d9865bf93a7dcbabd9a6cd32f95ae
Yihsiang Liow <yliow@ccis.edu>
2023-02-28 15:57:08 -0500

    no msg

--- 2023-02-28/main.cpp
+++ 2023-02-28/main.cpp
@@ -19,9 +19,12 @@ int main()
     
     const int BEGINNING_OF_ROAD = 1;
     const int SHACK = 2;
+    const int MIDDLE_OF_ROAD = 3;
     
     int my_pos = BEGINNING_OF_ROAD;
     bool sabrina_saved = false;
+    bool have_resolver = false;
+    bool have_silver_bullet = false;
     char input;
 
     for (int i = 0; i < 11; i = i + 1)
@@ -30,11 +33,15 @@ int main()
         if (my_pos == BEGINNING_OF_ROAD)
         {
             std::cout << "You are at the beginning of the road\n";
-            std::cout << "Options: q (quit), E (go east), ....\n";
+            std::cout << "Options: q (quit), E (go east), N (go north) ....\n";
             std::cin >> input;
             if (input == 'E')
             {
                 my_pos = SHACK;
+            }
+            else if (input == 'N')
+            {
+                my_pos = MIDDLE_OF_ROAD;
             }
             else if (input == 'q')
             {
@@ -50,6 +57,21 @@ int main()
             {
                 my_pos = BEGINNING_OF_ROAD;
             }
+        }
+        else if (my_pos == MIDDLE_OF_ROAD)
+        {
+            std::cout << "You are at the shack\n";
+            std::cout << "Options: S (go south), E (go east) ....\n";
+            std::cin >> input;
+            if (input == 'S')
+            {
+                my_pos = BEGINNING_OF_ROAD;
+            }
+            else if (input == 'E')
+            {
+                my_pos = SHACK;
+            }
+
         }
     }
     std::cout << "end of game!!!\n";
