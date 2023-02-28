04d249f9bd67d0a69f3730dd445f5c350444b1c0
Yihsiang Liow <yliow@ccis.edu>
2023-02-28 15:20:36 -0500

    no msg

--- 2023-02-28/main.cpp
+++ 2023-02-28/main.cpp
@@ -8,7 +8,7 @@
      /               |S             \
     /                |               \
    |                 |                |
-  N|                 |N               |N
+  N| E      W        |N     E         |N
 pond ------ beginning of road ------- shack
 
  */
@@ -16,6 +16,23 @@ pond ------ beginning of road ------- shack
 
 int main()
 {
+    const int BEGINNING_OF_ROAD = 1;
+    const int SHACK = 2;
+    
+    int my_pos = BEGINNING_OF_ROAD;
+    char input;
+
+    if (my_pos == BEGINNING_OF_ROAD)
+    {
+        std::cout << "You are at the beginning of the road\n";
+        std::cout << "Options: E (go east), ....\n";
+        std::cin >> input;
+        std::cout << "input is " << input << '\n';
+    }
+    else if (my_pos == SHACK)
+    {
+        std::cout << "You are at the shack\n";        
+    }
     
     return 0;
 }
