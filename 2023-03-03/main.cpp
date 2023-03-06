84d6ce48dcde683d375fc7d382d2ce18b13188aa
Yihsiang Liow <yliow@ccis.edu>
2023-03-06 14:32:42 -0500

    no msg

--- 2023-03-03/main.cpp
+++ 2023-03-03/main.cpp
@@ -1,16 +1,31 @@
 #include <iostream>
+#include <cmath>
 
 int main()
 {
-    int start, end;
-    std::cin >> start >> end;
+    // int start, end;
+    // std::cin >> start >> end;
     
-    std::cout << "n  " << ' ' << "n^2" << '\n';
-    std::cout << "---" << ' ' << "---" << '\n';
-    for (int i = start; i <= end; ++i)
+    // std::cout << "n  " << ' ' << "n^2" << '\n';
+    // std::cout << "---" << ' ' << "---" << '\n';
+    // for (int i = start; i <= end; ++i)
+    // {
+    //     std::cout << i << ' ' << i * i << '\n';
+    // }
+
+    // for (double x = 0.0; x < 2 * 3.14159; x += 0.01)
+    // {
+    //     std::cout << x << ' ' << sin(x) << '\n';
+    // }
+
+    for (int i = 10; i > 0; --i)
     {
-        std::cout << i << ' ' << i * i << '\n';
+        std::cout << i;
+        std::cout << " ... ";
     }
-        
+    std::cout << "blast off!\n";
+
+    std::cout << i << '\n';
+    
     return 0;
 }
