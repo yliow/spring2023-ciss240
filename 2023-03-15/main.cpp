86053470e41151f55d6a8624eed05b7637dbd9ab
Yihsiang Liow <yliow@ccis.edu>
2023-03-15 15:23:54 -0400

    no msg

--- 2023-03-15/main.cpp
+++ 2023-03-15/main.cpp
@@ -1,28 +1,37 @@
 #include <iostream>
+#include <cmath>
+
+
+bool isprime(int n)
+{
+    if (n < 2)
+    {
+        return false;
+    }
+    else
+    {
+        for (int d = 2; d <= sqrt(n); ++d)
+        {
+                if (n % d == 0)
+                {
+                    return false;
+                }
+        }
+    }
+    return true;
+}
+
+
+
+
+
 
 int main()
 {
     int numprimes = 1;
-    for (int n = 0; n <= 10000; ++n)
-    {
-        bool isprime = true;
-        if (n < 2)
-        {
-            isprime = false;
-        }
-        else
-        {
-            for (int d = 2; d < n; ++d)
-            {
-                if (n % d == 0)
-                {
-                    isprime = false;
-                    break;
-                }
-            }
-        }
-        
-        if (isprime)
+    for (int n = 900000000; n <= 1000000000; ++n)
+    {        
+        if (isprime(n))
         {
             std::cout << numprimes << ". " << n << '\n';
             ++numprimes;
