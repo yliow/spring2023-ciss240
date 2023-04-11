8ab6d8f2645238c7fd988f2feb2c475ec9e8cbd3
Yihsiang Liow <yliow@ccis.edu>
2023-04-11 16:00:59 -0400

    no msg

--- 2023-04-10/main.cpp
+++ 2023-04-10/main.cpp
@@ -7,7 +7,7 @@ void rand_array(int x[], int n)
 {
     for (int i = 0; i < n; ++i)
     {
-        x[i] = rand() % 1000;
+        x[i] = rand() % 50;
     }
     return;
 }
@@ -45,14 +45,47 @@ void bubblesort(int x[100000], int n)
 
 int main()
 {
-    srand(0);
-    int x[1000000];
-    int n = 10;
-    rand_array(x, n);
-    print_array(x, n);
-    bubblesort(x, n);
-    print_array(x, n);
+    // srand(0);
+    // int x[1000000];
+    // int n = 20;
+    // rand_array(x, n);
+    // print_array(x, n);
+    // bubblesort(x, n);
+    // print_array(x, n);
 
-    // bin search 
+    // int target;
+    // std::cin >> target;
+    
+    // // bin search
+    // int left = 0;
+    // int right = n - 1;
+    // int index = -1;
+    // while (left <= right)
+    // {
+    //     int mid = (left + right) / 2;
+    //     std::cout << "left,mid,right:"
+    //               << left << ' ' << mid << ' ' << right << '\n';
+    //     if (x[mid] == target)
+    //     {
+    //         std::cout << "found!\n";
+    //         index = mid;
+    //         break;
+    //     }
+    //     else if (x[mid] < target)
+    //     {
+    //         std::cout << "chase toward the right!\n";
+    //         left = mid + 1;
+    //     }
+    //     else
+    //     {
+    //         std::cout << "chase toward the left!\n";
+    //         right = mid - 1;
+    //     }
+    // }
+
+    char c = 'A';
+    std::cout << c << ' ' << int(c) << '\n';
+    std::cin >> c;
+    std::cout << c << ' ' << int(c) << '\n';
     return 0;
 }
