caba421fd7f5a45e9f2e9202068e03cdc2550294
Yihsiang Liow <yliow@ccis.edu>
2023-04-12 15:26:06 -0400

    no msg

---
+++ 2023-04-12/main.cpp
@@ -0,0 +1,63 @@
+#include <iostream>
+
+int main()
+{
+    // char c;
+    // std::cin >> c;
+    // std::cout << c << '\n';
+    // std::cout << int(c) << '\n';
+    // std::cout << 'Z' << ' ' << int('Z') << '\n';
+    // char d = 65;
+    // std::cout << d << '\n';
+
+    // for (int i = 65; i <= 90; ++i)
+    // {
+    //     std::cout << char(i);
+    // }
+    // std::cout << '\n';
+    
+    // for (int i = 97; i <= 97+25; ++i)
+    // {
+    //     std::cout << char(i);
+    // }
+    // std::cout << '\n';
+    // for (char x = 'a'; x <= 'z'; ++x)
+    // {
+    //     std::cout << x;
+    // }
+    // std::cout << '\n';
+
+    // // c = '3' -> i = 3
+    // // c = '9' -> i = 9
+    // char c;
+    // std::cin >> c;
+    // std::cout << c << '\n';
+    // int i = c - '0';
+    // std::cout << i << ' ' << i + 1 << '\n';
+
+    // std::cout << "hello world\n";
+    // char s[1024] = "dk$ben.ana aadfasdfaqwiugaoru\n";
+    // std::cout << s << '\n';
+    // for (int i = 0; i < 10; ++i)
+    // {
+    //     std::cout << s[i] << '\n';
+    // }
+
+    // // capitalize
+    // for (int i = 0; i < 10; ++i)
+    // {
+    //     if ('a' <= s[i] && s[i] <= 'z')
+    //     {
+    //         s[i] = s[i] - 'a' + 'A';
+    //     }
+    // }
+    // std::cout << s << '\n';
+
+    char s[1024] = {'h', 'e', 'l', 'l', 'o', '\0', 'w', 'o', 'r', 'l', 'd'};
+    std::cout << s << '\n';
+    std::cout << s[10] << '\n';
+    
+    return 0;
+}
+
+
