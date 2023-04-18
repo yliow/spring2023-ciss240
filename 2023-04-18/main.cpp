dff4d81464f574fce372a6c7665a24ca96f689bc
Yihsiang Liow <yliow@ccis.edu>
2023-04-18 15:37:43 -0400

    no msg

--- 2023-04-18/main.cpp
+++ 2023-04-18/main.cpp
@@ -1,14 +1,65 @@
 #include <iostream>
 
+void print_menu();
+void play();
+void help();
+
 int main()
 {
-    int i = 0, j = 1;
-
-    // swap
-    int t = i;
-    i = j;
-    j = t;
-    std::cout << i << ' ' << j << '\n';
-    
+    print_menu();
+    char option[1024];
+    std::cout << "option? ";
+    std::cin >> option;
+    switch (option[0])
+    {
+        case 'p':
+            play();
+            break;
+        case '?':
+            help();
+            break;
+        case 'q':
+            std::cout << "good bye!\n";
+            break;
+    }
     return 0;
 }
+
+void play()
+{
+    std::cout << "play() ... TODO\n";
+}
+
+void help()
+{
+    std::cout << "help() ... TODO\n";
+}
+
+void print_menu()
+{
+    std::cout << "welcome to ttt v 0.1\n"
+              << "by y.liow\n"
+              << "sent donations to yliow@ccis.edu\n"
+              << "[p] play game\n"
+              << "[?] help\n"
+              << "[q] quit\n";
+}
+
+// void swap(int & i, int & j);
+
+// int main()
+// {
+//     int i = 0, j = 1;
+//     swap(i, j);
+//     std::cout << i << ' ' << j << '\n';
+    
+//     return 0;
+// }
+
+// // swap
+// void swap(int & i, int & j)
+// {
+//     int t = i;
+//     i = j;
+//     j = t;
+// }
